fe84787cdab8e00ef6eaa465b0bb70639398b61f
Viktorija Panovska <35845054+viktorija-panovska@users.noreply.github.com>
2022-12-26 18:45:28 +0100

    Fixed camera controls

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -1,13 +1,20 @@
+
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Camera thisCamera;
 
-    private const float panSpeed = 1f;
-    private const float movementTime = 5f;
-    private const float rotationSpeed = 10f;
-    private const float zoomSpeed = 10f;
+    private const float movementTime = 50f;
+    private const float movementSpeed = 1f;
+
+    private const float zoomTime = 50f;
+    private const float zoomSpeed = 1f;
+    private const float minZoom = 30f;
+    private const float maxZoom = 120f;
+
+    private const float rotationTime = 5f;
+    private const float rotationSpeed = 1f;
 
 
     void Update()
@@ -18,33 +25,38 @@ public class CameraController : MonoBehaviour
     private void MoveCamera()
     {
         Vector3 newPosition = thisCamera.transform.position;
-        float newZoom = thisCamera.orthographicSize;
-        Quaternion newRotation = thisCamera.transform.rotation;
+        float newZoom = thisCamera.fieldOfView;
+        Quaternion newRotation = Quaternion.Euler(Vector3.up * thisCamera.transform.rotation.eulerAngles.y);
 
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            newPosition += (transform.forward * panSpeed);
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) 
-            newPosition += (transform.forward * -panSpeed);
+            newPosition += (transform.forward * movementSpeed);
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            newPosition += (transform.forward * -movementSpeed);
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            newPosition += (transform.right * panSpeed);
+            newPosition += (transform.right * movementSpeed);
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            newPosition += (transform.right * -panSpeed);
-
-
-        if (Input.GetKey(KeyCode.Q))
-            newRotation *= Quaternion.Euler(Vector3.up * rotationSpeed);
-        if (Input.GetKey(KeyCode.E))
-            newRotation *= Quaternion.Euler(Vector3.up * -rotationSpeed);
-
+            newPosition += (transform.right * -movementSpeed);
 
         if (Input.mouseScrollDelta.y != 0)
             newZoom += Input.mouseScrollDelta.y * zoomSpeed;
 
+        if (Input.GetKey(KeyCode.Q))
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationSpeed);
+        if (Input.GetKey(KeyCode.E))
+            newRotation *= Quaternion.Euler(Vector3.up * rotationSpeed);
+
+        newRotation = Quaternion.Euler(newRotation.eulerAngles + Vector3.right * thisCamera.transform.rotation.eulerAngles.x);
 
 
         // for smooth camera movement
-        thisCamera.transform.position = Vector3.Lerp(thisCamera.transform.position, newPosition, Time.deltaTime * movementTime);
-        thisCamera.transform.rotation = Quaternion.Lerp(thisCamera.transform.rotation, newRotation, Time.deltaTime * movementTime);
-        thisCamera.orthographicSize = Mathf.Lerp(thisCamera.orthographicSize, newZoom, Time.deltaTime);
+
+        if (newPosition != thisCamera.transform.position)
+            thisCamera.transform.position = Vector3.Lerp(thisCamera.transform.position, newPosition, Time.deltaTime * movementTime);
+
+        if (newZoom != thisCamera.fieldOfView)
+            thisCamera.fieldOfView = Mathf.Clamp(Mathf.Lerp(thisCamera.fieldOfView, newZoom, Time.deltaTime * movementTime), minZoom, maxZoom);
+
+        if (newRotation != thisCamera.transform.rotation)
+            thisCamera.transform.rotation = Quaternion.Lerp(thisCamera.transform.rotation, newRotation, Time.deltaTime * rotationTime);
     }
 }
